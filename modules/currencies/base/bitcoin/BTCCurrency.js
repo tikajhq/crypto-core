f72893857777416d4e40b1db4e9d36e36cfeed7e
Madhurendra Sachan <madhurendra@tikaj.com>
2018-03-19 00:18:42 +0530

    [Added] BTCCurrency on top of Currency Model, Allows to perform universal transactions.

---
+++ modules/currencies/base/bitcoin/BTCCurrency.js
@@ -0,0 +1,124 @@
+const Currency = require("./../../base/Currency");
+const RPCService = require("./../../base/bitcoin/RPCService");
+const ChainSo = require("./ChainSo");
+
+let bitcoinjs = require('bitcoinjs-lib');
+let CoinKey = require('coinkey');
+let coinInfo = require('coininfo');
+
+
+class BTCCurrency extends Currency {
+    constructor(notation) {
+        super(notation);
+        this.currencyInfo = coinInfo(this.notation).versions;
+        this.networkInfo = coinInfo(this.notation).toBitcoinJS();
+        this.divisionFactor = 100000000;
+
+        //if its one of currency supported by chain.so, use it.
+        if (['btc', 'ltc', 'doge', 'dash'].indexOf(notation) !== -1)
+            this.HTTPAPI = new ChainSo(notation)
+    }
+
+    normalizeToSatoshis(value) {
+        return Math.round(value * this.divisionFactor);
+    }
+
+    waitForConfirmation(tx, rawtx) {
+        return this.api.addTXForConfirmation([tx.txid]);
+    }
+
+    listenForIncomingTX() {
+        //register
+        this.api.newTXService();
+        this.api.confirmationService();
+
+        //on new received.
+        this.api.on('transaction', (tx) => {
+            this.onUTXO(this.api.normalizeTX(tx), tx);
+        });
+        this.api.on("confirmed", (tx) => {
+            this.onConfirmedTX(this.api.normalizeTX(tx), tx)
+        })
+    }
+
+    generateWallet() {
+        let ck = new CoinKey.createRandom(this.currencyInfo);
+        return {
+            address: ck.publicAddress,
+            secret: ck.privateWif,
+            key: ck.privateKey.toString('hex'),
+        }
+
+    }
+
+
+    transfer(transaction, cb) {
+        const self = this;
+
+        transaction.fee = (transaction.fee || this.fee);
+        //convert value to satoshis
+        let value = transaction.value;
+        this.HTTPAPI.getUTXO(transaction.source.address, (err, outputs) => {
+            if (err) {
+                console.error("An error occured while transferring from " + transaction.source.address + " to " + transaction.destination + " value:" + value);
+                console.error(err);
+            }
+
+            if (outputs.length === 0) {
+                return console.log("Wallet is empty.");
+            }
+
+            let netBalance = 0;
+            let tx = new bitcoinjs.TransactionBuilder(this.networkInfo);
+
+
+            // add each utxo as input.
+            outputs.forEach((utxo) => {
+                netBalance += utxo.value;
+                tx.addInput(utxo.txid, utxo.index);
+            });
+
+            // see if balance is upto mark.
+            if (netBalance < (value + transaction.fee))
+                return console.log("Wallet balance is less than transfer. " + netBalance);
+
+
+            /**
+             * Calculations
+             */
+            tx.addOutput(transaction.destination, self.normalizeToSatoshis(value));
+
+            //check if anything remains after this tx.
+            let remaining = netBalance - value - transaction.fee;
+            if (remaining > transaction.fee)
+                tx.addOutput(transaction.source.address, self.normalizeToSatoshis(remaining));
+
+
+            /**
+             * Section for signing & submission
+             */
+                //get the private key/
+            let keyPair = bitcoinjs.ECPair.fromWIF(transaction.source.secret, this.networkInfo);
+
+            // sign all inputs with valid key.
+            for (let i = 0; i < outputs.length; ++i) {
+                // Sign the first input with the key.
+                tx.sign(i, keyPair);
+            }
+            //get raw tx
+            let txHex = tx.build().toHex();
+            console.log(txHex);
+            if (this.ACTUAL_TRANSFER) {
+                self.api.broadcastTX(txHex, (error, response) => {
+                    if (error) console.log(error);
+                    console.log(response);
+                    cb(err, response);
+                });
+            } else
+                cb(null, txHex);
+        });
+    }
+}
+
+
+module.exports = BTCCurrency;
