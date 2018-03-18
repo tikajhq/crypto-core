a198a8d6cb7108e73885aea62d36027896ef8305
Madhurendra Sachan <madhurendra@tikaj.com>
2018-03-19 00:22:48 +0530

    [Update] Updates DASH Model.

--- modules/currencies/crypto/dash/index.js
+++ modules/currencies/crypto/dash/index.js
@@ -1,15 +1,10 @@
-const Currency = require("./../../base/Currency");
+const Currency = require("./../../base/bitcoin/BTCCurrency");
 const RPCService = require("./../../base/bitcoin/RPCService");
-
-let bitcoin = require('bitcoinjs-lib');
-let CoinKey = require('coinkey');
-let coinInfo = require('coininfo');
-
 
 class Doge extends Currency {
     constructor(options, cb) {
         super("dash");
-        this.currencyInfo = coinInfo(this.notation).versions;
+        this.divisionFactor = 1;
         this.api = new RPCService({
             currency: this.notation,
             rpcConfig: {
@@ -20,44 +15,6 @@ class Doge extends Currency {
                 port: '9998',
             }
         }, cb);
-    }
-
-    waitForConfirmation(tx, rawtx) {
-        return this.api.addTXForConfirmation([tx.txid]);
-    }
-
-    listenForIncomingTX() {
-        //register
-        this.api.newTXService();
-        this.api.confirmationService();
-
-        //on new received.
-        this.api.on('transaction', (tx) => {
-            this.onUTXO(this.api.normalizeTX(tx), tx);
-        });
-        this.api.on("confirmed", (tx) => {
-            this.onConfirmedTX(this.api.normalizeTX(tx), tx)
-        })
-    }
-
-
-    generateWallet() {
-        let ck = new CoinKey.createRandom(this.currencyInfo);
-        return {
-            address: ck.publicAddress,
-            secret: ck.privateWif,
-            key: ck.privateKey.toString('hex'),
-        }
-
-    }
-
-    send(to, value, cb) {
-        //TODO: LOG
-        return this.transfer(this.primaryWallet, to, value, cb);
-    }
-
-    transfer(source, destination, value, cb) {
-
     }
 }
 
