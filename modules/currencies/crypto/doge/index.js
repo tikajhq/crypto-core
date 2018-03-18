fcd20f46d6b0a1fe1a8ff4ddc95c6cfc3c0d5575
Madhurendra Sachan <madhurendra@tikaj.com>
2018-03-19 00:23:26 +0530

    [Update] Updated DOGE Model, can stream info now.

--- modules/currencies/crypto/doge/index.js
+++ modules/currencies/crypto/doge/index.js
@@ -1,15 +1,9 @@
-const Currency = require("./../../base/Currency");
+const Currency = require("./../../base/bitcoin/BTCCurrency");
 const RPCService = require("./../../base/bitcoin/RPCService");
-
-let bitcoin = require('bitcoinjs-lib');
-let CoinKey = require('coinkey');
-let coinInfo = require('coininfo');
-
 
 class Doge extends Currency {
     constructor(options, cb) {
         super("doge");
-        this.currencyInfo = coinInfo(this.notation).versions;
         this.api = new RPCService({
             currency: this.notation,
             rpcConfig: {
@@ -20,44 +14,9 @@ class Doge extends Currency {
                 port: '22555',
             }
         }, cb);
-    }
+        this.fee = 0.01;
 
-    waitForConfirmation(tx, rawtx) {
-        return this.api.addTXForConfirmation([tx.txid]);
-    }
-
-    listenForIncomingTX() {
-        //register
-        this.api.newTXService();
-        this.api.confirmationService();
-
-        //on new received.
-        this.api.on('transaction', (tx) => {
-            this.onUTXO(this.api.normalizeTX(tx), tx);
-        });
-        this.api.on("confirmed", (tx) => {
-            this.onConfirmedTX(this.api.normalizeTX(tx), tx)
-        })
-    }
-
-
-    generateWallet() {
-        let ck = new CoinKey.createRandom(this.currencyInfo);
-        return {
-            address: ck.publicAddress,
-            secret: ck.privateWif,
-            key: ck.privateKey.toString('hex'),
-        }
-
-    }
-
-    send(to, value, cb) {
-        //TODO: LOG
-        return this.transfer(this.primaryWallet, to, value, cb);
-    }
-
-    transfer(source, destination, value, cb) {
-
+        this.networkInfo.messagePrefix = '\x19Dogecoin Signed Message:\n';
     }
 }
 
