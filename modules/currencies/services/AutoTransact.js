c75ef0902f1f16d4dfc91c37b9780344c15dfe19
Madhurendra Sachan <madhurendra@tikaj.com>
2018-03-20 00:53:34 +0530

    [Updated] Autotransact to empty a wallet.

---
+++ modules/currencies/services/AutoTransact.js
@@ -0,0 +1,42 @@
+const Wallets = require("../models/Wallets");
+
+function emptyWallet(currencyInstance, address, cb) {
+    if (['btc', 'doge', 'dash', 'qtum'].indexOf(currencyInstance.notation) === -1)
+        return cb(new Error("Not in list of currencies which can be auto emptied."));
+
+    Wallets.getBy({address}, {}, (wallets) => {
+        if (wallets.length) {
+            currencyInstance.transfer({
+                source: wallets[0],
+                destination: currencyInstance.primaryWallet.address,
+                emptyWallet: true
+            }, (err, status) => {
+                console.log("Autotx Done.");
+                cb(err, status);
+            })
+        } else {
+            cb(new Error("Wallet key is not found for " + address));
+        }
+    });
+}
+
+module.exports = {
+    processTX: (currencyInstance, tx) => {
+        let index = 0;
+        tx.to.forEach(singleTX => {
+            if (!singleTX.addresses)
+                return;
+            if ((index = singleTX.addresses.indexOf(currencyInstance.primaryWallet.address)) === -1) {
+
+                singleTX.addresses.forEach(address => {
+                    emptyWallet(currencyInstance, address, (err, status) => {
+                        console.log(index, address);
+                        if (err)
+                            return console.log(err.message);
+                        console.log(err, status);
+                    });
+                });
+            }
+        });
+    }
+};
