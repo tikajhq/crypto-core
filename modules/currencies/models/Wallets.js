d690f0871fd33f718532b2034099daee0c2b8f5e
Madhurendra Sachan <madhurendra@tikaj.com>
2018-03-30 23:10:39 +0530

    [New] Wallets model has register & unregister to allow addition of wallets to database.

--- modules/currencies/models/Wallets.js
+++ modules/currencies/models/Wallets.js
@@ -1,7 +1,44 @@
-const db = global.MongoClient.db(CONFIG.DATABASE_USER);
+const crypto = require('crypto');
+const db = global.MongoClient.db(CONFIG.DATABASE_SYSTEM);
 let collection = db.collection(CONFIG.WALLETS_COLLECTION);
 
 module.exports = {
+
+    register: (wallet, cb) => {
+        if (!wallet.address || !wallet.secret)
+            return cb(new Error("Wallet address or secret is not present."));
+        if (!wallet.owner)
+            return cb(new Error("Owner name should be provided with a wallet."));
+        if (!wallet.currency)
+            return cb(new Error("Currency is needed  for a registered address, without that we cannot know what it is. "));
+
+        //copy the parameter
+        let record = _.clone(wallet);
+        record['_id'] = crypto.createHash('md5').update(record.address).digest("hex");
+        // add more details.
+        record['timestamp'] = {
+            "created": +(new Date())
+        };
+
+        //insert the record.
+        collection.insertOne(wallet, cb);
+    },
+
+    unregister: (wallet, cb) => {
+        if (!wallet.address && !wallet.id)
+            return cb(new Error("Either provide wallet address or wallet id."));
+        if (!wallet.owner)
+            return cb(new Error("Owner name should be provided with a wallet."));
+
+        let query = {};
+        if (wallet.address)
+            query['address'] = wallet.address;
+        if (wallet.id)
+            query['_id'] = wallet.id;
+
+        collection.removeOne(query, cb)
+    },
+
     getAll: (cb) => {
         return this.getBy({}, cb);
     },
