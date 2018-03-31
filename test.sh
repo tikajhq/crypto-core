96661f3038b38c98b30961637e753746f2602d7a
Madhurendra Sachan <madhurendra@tikaj.com>
2018-03-31 11:53:49 +0530

    [Update] Wait for sometime before running a next test.

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@
 test(){
   NAME=$1
   mocha ./modules/currencies/tests/$NAME.js -vv --timeout=20000 --reporter mochawesome --reporter-options reportDir=dev/docs/tests/$NAME,reportFilename=index
+  sleep 2
 }
-test SuperResolver
-test Currencies
+
 test HTTPAPIs
+test Currencies
+test SuperResolver
