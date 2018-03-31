aedddecf8c3a223817689cb8f2ce777bb3676055
Madhurendra Sachan <madhurendra@tikaj.com>
2018-03-31 12:00:59 +0530

    [Update] Fix path of tests.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 test(){
   NAME=$1
-  mocha ./modules/currencies/tests/$NAME.js -vv --timeout=20000 --reporter mochawesome --reporter-options reportDir=dev/docs/tests/$NAME,reportFilename=index
+  mocha ./modules/currencies/tests/$NAME.js -vv --timeout=20000 --reporter mochawesome --reporter-options reportDir=dev/docs/tests/,reportFilename=$NAME
   sleep 2
 }
 
